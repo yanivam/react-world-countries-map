bc77fa5134801b167ec45be7970519f0bcd2297a
Yaniv Amiri <yanivam123@gmail.com>
2020-03-23 19:38:26 -0400

    Added css for map and zoomed out initially, added data to app.tsx as a sample, and made it so that all of the countries with data get highlighted

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ReactWorldMap color="#ff0000" title="My chart" data=[{"country-code":"us", value:360}]/>
+        <ReactWorldMap color="#ff0000" value-title="My Chart" data=
+        {
+          [
+            {
+              "country-code":"us", 
+              value:360
+            },
+            {
+              "country-code":"ru", 
+              value:200
+            },
+            {
+              "country-code":"il", 
+              value:230
+            }
+          ]
+        }
+        />
       </header>
     </div>
   );
--- src/ReactWorldMap/ReactWorldMap.tsx
+++ src/ReactWorldMap/ReactWorldMap.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react"
+import React from "react"
 import { geoMercator, geoPath } from "d3-geo"
 import geoData from "../data/countries.geo"
+import "./ReactWorldMap.css"
 
 interface IData {
   "country-code": string;
@@ -18,40 +19,59 @@ interface IProps {
 const ReactWorldMap:React.FC<IProps> = (props) => {
 
   // Declare a new state variable, which we'll call "count"
-  const [color, setColor] = useState("#dedede");
-  const [countryValueMap, setCountryValueMap] = useState([]);
+  //const [color, setColor] = useState("#dedede");
+  //const [countryValueMap, setCountryValueMap] = useState([]);
   const projection = geoMercator()
-  
+  .scale(75)
   const pathGenerator = geoPath().projection(projection)
-  var showTooltip = false
+  /*var showTooltip = false
   var clickedCountryName = ""
-  var clickedCountryValue = 0
+  var clickedCountryValue = 0*/
+
+  /*const computedAverage = (data: IData[]) => {
+    var sum = data.reduce((sum, value) => {
+      return sum + value;
+    }, 0);
+  
+    return avg = sum / data.length;
+  }*/
+
+  const countryWithValue = (iso: string) => {
+    for(let countryObject of props.data) {
+        if(iso.toLowerCase() === countryObject["country-code"].toLowerCase()) {
+           return true
+        }
+    }
+    return false
+  }
   
   const countries = geoData.features
       .map((feature, idx) => {
-          const averageValue = computedAverage(countryValueMap)
-          const standardDeviation = computedStandardDeviation(countryValueMap)
-          const opacityLevel = countryValueMap.contains(feature.properties.ADM0_A3) ? computedPercentage(countryValueMap[feature.properties.ADM0_A3], averageValue, standardDeviation) : 0
+          //const averageValue = computedAverage(props.data.values)
+          //const standardDeviation = computedStandardDeviation(props.data.values)
+          const color = countryWithValue(feature.properties.ISO_A2) ? props.color : "#dddddd"
           return <path
               key={"path" + idx}
               d={pathGenerator(feature as GeoJSON.Feature) as string}
-              style={{ fill: color, fillOpacity: opacityLevel, stroke: "black", strokeWidth: 2, strokeOpacity: 1, cursor: "pointer" }}
-              className="states"
+              style={{ fill: color, fillOpacity: 1, stroke: "black", strokeWidth: 1, strokeOpacity: 0.1, cursor: "pointer" }}
+              className="countries"
       />})
 
   return (
-    <div className="map">
-        <svg width={"100%"} height={"100%"}>
-            <g id="highlighter" className="highlighter" onClick={toggleTooltip(showTooltip)} >
-              {countries}
-            </g>
-            <div className={showTooltip ? "show" : "hide"}>
-              <p>Country name: {clickedCountryName}</p>
-              <p>Value: {clickedCountryValue}</p>
-            </div>
+    <div className="mapView">
+        <svg className="map" width={"100%"} height={"100%"}>
+        {countries}
         </svg>
     </div>
   )
+  /*
+  <g id="highlighter" className="highlighter" onClick={toggleTooltip()} >
+    {countries}
+  </g>
+  <div className={showTooltip ? "show" : "hide"}>
+    <p>Country name: {clickedCountryName}</p>
+    <p>Value: {clickedCountryValue}</p>
+  </div>*/
 }
 
 export default ReactWorldMap
