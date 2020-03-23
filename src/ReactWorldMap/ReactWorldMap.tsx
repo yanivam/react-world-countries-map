078fac3f46d231fab94ebf41151514187ad25194
Yaniv Amiri <yanivam123@gmail.com>
2020-03-23 01:55:08 -0400

    Psuedocode for what I want project to be like once I learn more typescript (haven't written the functions yet)

--- src/ReactWorldMap/ReactWorldMap.tsx
+++ src/ReactWorldMap/ReactWorldMap.tsx
@@ -4,24 +4,37 @@ import geoData from "../data/countries.geo"
 
 const ReactWorldMap = () => {
   // Declare a new state variable, which we'll call "count"
+  const [color, setColor] = useState("#dedede");
+  const [countryValueMap, setCountryValueMap] = useState([]);
   const projection = geoMercator()
   
   const pathGenerator = geoPath().projection(projection)
+  var showTooltip = false
+  var clickedCountryName = ""
+  var clickedCountryValue = 0
   
   const countries = geoData.features
       .map((feature, idx) => {
-          const fakeData = Math.random()
+          const averageValue = computedAverage(countryValueMap)
+          const standardDeviation = computedStandardDeviation(countryValueMap)
+          const opacityLevel = countryValueMap.contains(feature.properties.ADM0_A3) ? computedPercentage(countryValueMap[feature.properties.ADM0_A3], averageValue, standardDeviation) : 0
           return <path
               key={"path" + idx}
               d={pathGenerator(feature as GeoJSON.Feature) as string}
-              style={{ fill: "#dedede", fillOpacity: fakeData, stroke: "black", strokeWidth: 2, strokeOpacity: 1, cursor: "pointer" }}
+              style={{ fill: color, fillOpacity: opacityLevel, stroke: "black", strokeWidth: 2, strokeOpacity: 1, cursor: "pointer" }}
               className="states"
       />})
 
   return (
     <div className="map">
         <svg width={"100%"} height={"100%"}>
-            {countries}
+            <g id="highlighter" className="highlighter" onClick={toggleTooltip(showTooltip)} >
+              {countries}
+            </g>
+            <div className={showTooltip ? "show" : "hide"}>
+              <p>Country name: {clickedCountryName}</p>
+              <p>Value: {clickedCountryValue}</p>
+            </div>
         </svg>
     </div>
   )
