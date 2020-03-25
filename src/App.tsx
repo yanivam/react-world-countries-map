b334d9316dc3792579e0cbdab681adb01625f3d8
ez <ez@skippy.home>
2020-03-25 08:34:49 -0400

    cleanups

--- src/App.tsx
+++ src/App.tsx
@@ -5,51 +5,25 @@ import ReactWorldMap from "./ReactWorldMap"
 function App() {
   const data =
     [
-      {
-        "country-code": "us",
-        value: 10
-      },
-      {
-        "country-code": "ru",
-        value: 20
-      },
-      {
-        "country-code": "ca",
-        value: 30
-      },
-      {
-        "country-code": "mx",
-        value: 40
-      },
-      {
-        "country-code": "fr",
-        value: 50
-      }
+      {"country-code": "cn", value: 1389618778}, // china
+      {"country-code": "id", value: 1311559204}, // india
+      {"country-code": "us", value: 331883986},  // united states
+      {"country-code": "in", value: 264935824},  // indonesia
+      {"country-code": "pk", value: 210797836},  // pakistan
+      {"country-code": "br", value: 210301591},  // brazil
+      {"country-code": "ng", value: 208679114},  // nigeria
+      {"country-code": "bd", value: 161062905},  // bangladesh
+      {"country-code": "ru", value: 141944641},  // russia
+      {"country-code": "mx", value: 127318112}   // mexico
     ]
 
   return (
     < div className="App" >
       < div className="Main">
-        <ReactWorldMap color="#00ff00" title="My Chart" value-label="Smart people" size="xl" data={data} />
+        <ReactWorldMap color="#00ff00" title="Top 10 Populous Countries" value-suffix="people" size="md" data={data} />
       </div>
     </div>
   );
 }
 
 export default App;
-
-/*
-        <br/>
-        <h1>size=lg</h1>
-        <ReactWorldMap color="#ff0000" title="My Chart" size="lg" data={data} />
-        <br/>
-        <h1>size=md</h1>
-        <ReactWorldMap color="#ff0000" title="My Chart" size="md" data={data} />
-        <br/>
-        <h1>size=sm</h1>
-        <ReactWorldMap color="#ff0000" title="My Chart" size="sm" data={data} />
-        <br/>
-        <h1>size=xs</h1>
-        <ReactWorldMap color="#ff0000" title="My Chart" size="xs" data={data} />
-        <br/>
-*/
--- src/ReactWorldMap/ReactWorldMap.tsx
+++ src/ReactWorldMap/ReactWorldMap.tsx
@@ -14,8 +14,11 @@ interface IData {
 interface IProps {
   data: IData[],
   title?: string,
-  "value-label"?: string,
+  "value-prefix"?: string,
+  "value-suffix"?: string,
   color?: string,
+  tooltipBgColor?: string,
+  tooltipTextColor?: string,
   size?: string // possile values are xs, sm, md, lg, xl
 }
 
@@ -25,29 +28,33 @@ interface ISize {
   viewBox: string
 }
 
-const CSizes: {[key:string]:ISize} = {
-  "xs": {width: 146, height: 132, viewBox: "0 -200 960 760"}, //screen size: 480x360
-  "sm": {width: 240, height: 200, viewBox: "0 -200 960 760"}, //screen size: 576x432
-  "md": {width: 336, height: 280, viewBox: "0 -200 960 760"}, //screen size: 768x576
-  "lg": {width: 480, height: 400, viewBox: "0 -200 960 760"}, //screen size: 992x744
-  "xl": {width: 720, height: 600, viewBox: "0 -200 960 760"}, //screen size: 1200x900
+const CSizes: { [key: string]: ISize } = {
+  "xs": { width: 146, height: 132, viewBox: "0 -200 960 760" }, //screen size: 480x360
+  "sm": { width: 240, height: 200, viewBox: "0 -200 960 760" }, //screen size: 576x432
+  "md": { width: 336, height: 280, viewBox: "0 -200 960 760" }, //screen size: 768x576
+  "lg": { width: 480, height: 400, viewBox: "0 -200 960 760" }, //screen size: 992x744
+  "xl": { width: 720, height: 600, viewBox: "0 -200 960 760" }, //screen size: 1200x900
 }
 
 const ReactWorldMap: React.FC<IProps> = (props) => {
 
-  // Setup width and height based on size
-  const size = typeof(props.size)!=="undefined" ? props.size : "xs"
+  // Inits
+  const size = typeof (props.size) !== "undefined" ? props.size : "xs"
   const width = CSizes[size].width
   const height = CSizes[size].height
   const viewBox = CSizes[size].viewBox
+  const valuePrefix = (typeof(props["value-prefix"])==="undefined") ? "" : props["value-prefix"]
+  const valueSuffix = (typeof(props["value-suffix"])==="undefined") ? "" : props["value-suffix"]
+  const tooltipBgColor = (typeof(props.tooltipBgColor)==="undefined") ? "black" : props.tooltipBgColor
+  const tooltipTextColor = (typeof(props.tooltipTextColor)==="undefined") ? "white" : props.tooltipTextColor
 
   const containerRef = React.createRef<SVGSVGElement>();
 
-  // Build the country map for direct access
-  const countryValueMap: {[key:string]:number} = {}
-  let max:number = -Infinity
-  let min:number = Infinity
-  props.data.forEach( entry => {
+  // Calc min/max values and build country map for direct access
+  const countryValueMap: { [key: string]: number } = {}
+  let max: number = -Infinity
+  let min: number = Infinity
+  props.data.forEach(entry => {
     const key = entry["country-code"].toUpperCase()
     const value = entry.value
     min = (min > value) ? value : min
@@ -55,19 +62,40 @@ const ReactWorldMap: React.FC<IProps> = (props) => {
     countryValueMap[key] = value
   })
 
+  // Build path for each country
   const projection = geoMercator()
   const pathGenerator = geoPath().projection(projection)
 
-  // build path for each country
   const countriesPath = geoData.features
     .map((feature, idx) => {
-      let color:string = CDefaultColor
+      const triggerRef = React.createRef<SVGPathElement>();
+      const isHighlight = typeof (countryValueMap[feature.properties.ISO_A2]) != "undefined"
+      let color: string = CDefaultColor
       let opacityLevel = 0.1
-      if(typeof(countryValueMap[feature.properties.ISO_A2])!="undefined") {
+
+      // Things to do if country is in data
+      if (isHighlight) {
         color = props.color ? props.color : CDefaultColor
         opacityLevel += (0.9 * (countryValueMap[feature.properties.ISO_A2] - min) / (max - min))
       }
-      const triggerRef = React.createRef<SVGPathElement>();
+
+      const tooltip = (!isHighlight) ? "" :
+        <Tooltip triggerRef={triggerRef} containerRef={containerRef} >
+          <rect
+            x={10}
+            y={10}
+            width={300}
+            height={50}
+            rx={0.5}
+            ry={0.5}
+            fill={tooltipBgColor}
+          />
+          <text x={25} y={25} fontSize={12} fill={tooltipTextColor}>
+            <tspan>{feature.properties.NAME}: {countryValueMap[feature.properties.ISO_A2]} {valueSuffix}</tspan>
+          </text>
+        </Tooltip>
+
+      // Build a path for a country
       const path = (
         <g key={feature.properties.NAME}>
           <path
@@ -76,31 +104,16 @@ const ReactWorldMap: React.FC<IProps> = (props) => {
             d={pathGenerator(feature as GeoJSON.Feature) as string}
             style={{ fill: color, fillOpacity: opacityLevel, stroke: "black", strokeWidth: 1, strokeOpacity: 0.1, cursor: "pointer" }}
           />
-          <Tooltip triggerRef={triggerRef} containerRef={containerRef}>
-          <rect
-            x={10}
-            y={10}
-            width={300}
-            height={50}
-            rx={0.5}
-            ry={0.5}
-            fill="black"
-          />
-          <text x={25} y={25} fontSize={12} fill="white">
-            <tspan>Country name: {feature.properties.NAME}</tspan>
-            <tspan x={22} dy="1em">
-            {props["value-label"] ? props["value-label"]: "Value"}: {countryValueMap[feature.properties.ISO_A2] ? countryValueMap[feature.properties.ISO_A2] : 0}
-            </tspan>
-          </text>
-          </Tooltip>
+          {tooltip}
         </g>
       )
       return path
     })
 
+  // Render the SVG
   return (
     <div className="mapView">
-      <svg ref={containerRef} className="map" height={height+"px"} width={width+"px"} viewBox={viewBox}>
+      <svg ref={containerRef} className="map" height={height + "px"} width={width + "px"} viewBox={viewBox}>
         {countriesPath}
       </svg>
     </div>
