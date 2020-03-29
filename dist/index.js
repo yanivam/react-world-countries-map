807d64f18cea1fcb9e808eafcca5237594031834
ezborgy <49008885+ezborgy@users.noreply.github.com>
2020-03-29 17:57:00 -0400

    Delete index.js

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./react-world-countries-map.cjs.production.min.js')
-} else {
-  module.exports = require('./react-world-countries-map.cjs.development.js')
-}
